b2db3e8e6d36e3c7a43f2ad1862ebcb474cf54e0
Diana-nyamai <dnyamai.dn@gmail.com>
2021-08-09 20:08:22 +0300

    update

--- PHASE1/Practice/utils.js
+++ PHASE1/Practice/utils.js
@@ -2,6 +2,3 @@ function changeMessage(message){
       document.getElementById("message").textContent = message;
 }
 
-function displayReview(review){
-    document.getElementById('see-review').textContent = review
-}
